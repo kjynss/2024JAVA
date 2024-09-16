4955fad6af581ac87cdc6aefd1582bbaaf5cd29c
kjynss <101390983+kjynss@users.noreply.github.com>
2024-09-16 16:44:27 +0900

    20240916

---
+++ Ch2/src/jmk/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,16 @@
+package jmk.sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value;
+		
+		value = 10;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}
