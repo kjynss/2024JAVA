52763ce26e289620f199b829913a9a650dbce1fc
kjynss <101390983+kjynss@users.noreply.github.com>
2024-09-19 16:44:34 +0900

    20240919_2

---
+++ Ch3/src/jmk/sec02/exam03/DenyLogicOperation.java
@@ -0,0 +1,17 @@
+package jmk.sec02.exam03;
+
+public class DenyLogicOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+
+}
---
+++ Ch3/src/jmk/sec02/exam07/CompareOperatorExample1.java
@@ -0,0 +1,10 @@
+package jmk.sec02.exam07;
+
+public class CompareOperatorExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
