649ece295cb371bf376e22ff0da87b1b1248ca52
kjynss <101390983+kjynss@users.noreply.github.com>
2024-09-23 11:19:53 +0900

    20240923

---
+++ Ch3/src/jmk/sec02/exam10/AssignmentOperatorExmaple.java
@@ -0,0 +1,20 @@
+package jmk.sec02.exam10;
+
+public class AssignmentOperatorExmaple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = 0;
+		result += 10;
+		System.out.println("result=" + result);
+		result -= 5;
+		System.out.println("result=" + result);
+		result *= 3;
+		System.out.println("result=" + result);
+		result /= 5;
+		System.out.println("result=" + result);
+		result %= 3;
+		System.out.println("result=" + result);
+	}
+
+}
