2b9deff2b52e7bee7a30b2438a0a7e9de3a06630
kjynss <101390983+kjynss@users.noreply.github.com>
2024-11-05 13:37:55 +0900

    20241105_2

---
+++ Ch7/src/jmk/sec01/exam04/Airplane.java
@@ -0,0 +1,15 @@
+package jmk.sec01.exam04;
+
+public class Airplane {
+	public void land() {
+		System.out.println("착륙합니다.");
+	}
+	
+	public void fly() {
+		System.out.println("일반비행합니다.");
+	}
+	
+	public void takeOff() {
+		System.out.println("이륩합니다.");
+	}
+}
