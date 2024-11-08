940dbeed813f0b174281a3fe28c2049412927db7
kjynss <101390983+kjynss@users.noreply.github.com>
2024-11-08 11:34:03 +0900

    20241108

--- Ch7/src/ex02/Animal.java
+++ Ch7/src/ex02/Animal.java
@@ -1,7 +1,10 @@
 package ex02;
 
-public class Animal {
-	public boolean IsHealthy() {
-		return true;
-	}
+public abstract class Animal {
+	public abstract boolean IsHealthy();
+	// 추상클래스 실질적으로 사용되지는 않고 자식들을 묶어주는 역할?
+	// 추상메소드 : 함수에 바디가 없음. 리턴타입과 매개변수만 있음.
+	// abstract 로 추상 클래스 또는 메소드로 선언
+	// 추상클래스는 객체를 만들 수 없음.
+	// 필드는 가질 수 있음.
 }
