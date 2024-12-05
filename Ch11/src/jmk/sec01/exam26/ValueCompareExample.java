58adb9c8cdc30cd251dcd40e7cb7ec6701bf414a
kjynss <101390983+kjynss@users.noreply.github.com>
2024-12-06 01:14:52 +0900

    20241206

--- Ch11/src/jmk/sec01/exam26/ValueCompareExample.java
+++ Ch11/src/jmk/sec01/exam26/ValueCompareExample.java
@@ -13,7 +13,13 @@ public class ValueCompareExample {
 		System.out.println("equals() 결과: " + obj1.equals(obj2));
 		System.out.println();
 		
+		System.out.println("-128~127 초과값일 경우]");
+		Integer obj3 = 10;
+		Integer obj4 = 10;
 		
+		System.out.println("==결과: " + (obj3 == obj4));
+		System.out.println("언박싱후 ==결과: " + (obj3.intValue() == obj4.intValue()));
+		System.out.println("equals() 결과: " + obj3.equals(obj4));
 	}
 
 }
